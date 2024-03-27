e077e07c69610477971609769d6a7f19ad7aa233
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-03-26 21:27:15 -0400

    Fixed merge

---
+++ frontend/src/components/Town/interactables/PizzaParty/PizzaPartyArea.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { useInteractableAreaController } from '../../../../classes/TownController';
+import PizzaPartyAreaController from '../../../../classes/interactable/PizzaPartyAreaController';
+import useTownController from '../../../../hooks/useTownController';
+import { InteractableID } from '../../../../types/CoveyTownSocket';
+import PizzaPartyGame from './PizzaPartyGame';
+
+export default function PizzaPartyArea({
+  interactableID,
+}: {
+  interactableID: InteractableID;
+}): JSX.Element {
+  const gameAreaController =
+    useInteractableAreaController<PizzaPartyAreaController>(interactableID);
+  const townController = useTownController();
+
+  return (
+    <div>
+      <h1>Pizza Party Game</h1>
+      <PizzaPartyGame gameAreaController={gameAreaController} />
+    </div>
+  );
+}
