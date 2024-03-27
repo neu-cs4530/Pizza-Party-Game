908b81907c17604283bb351bd09924fb32746d3a
Dhivas-Sugumar <dhivassugumar@gmail.com>
2024-03-26 21:41:50 -0400

    Please fix it

--- frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
+++ frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import pizzaPartyImage from '../../../../../public/assets/pizza-party/background.png';
 import Image from 'next/image';
 import PizzaPartyAreaController from '../../../../classes/interactable/PizzaPartyAreaController';
 
@@ -12,7 +11,7 @@ export type PizzaPartyGameProps = {
 export default function PizzaPartyGame({ gameAreaController }: PizzaPartyGameProps): JSX.Element {
   return (
     <div>
-      <Image src={pizzaPartyImage} alt='Pizza Party Game' />
+      <Image src="../../../../../public/assets/pizza-party/background.png" alt='Pizza Party Game' />
     </div>
   );
 }
