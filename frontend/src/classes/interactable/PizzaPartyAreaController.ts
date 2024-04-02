a2af8f9c38b1bd4258ceaa920fe6003dd4f832e8
sho-r24 <rakhangi.s@northeastern.edu>
2024-04-02 16:34:11 -0400

    changed difficulty to 1

--- frontend/src/classes/interactable/PizzaPartyAreaController.ts
+++ frontend/src/classes/interactable/PizzaPartyAreaController.ts
@@ -27,7 +27,7 @@ export default class PizzaPartyAreaController extends GameAreaController<
       cooked: false,
       isInOven: false,
     },
-    difficulty: 2,
+    difficulty: 1,
   };
 
   public isActive(): boolean {
