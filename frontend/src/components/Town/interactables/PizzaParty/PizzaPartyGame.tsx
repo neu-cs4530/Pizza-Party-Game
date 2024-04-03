8321f7396ef9806fff70e187437e89607f119012
sho-r24 <rakhangi.s@northeastern.edu>
2024-04-02 21:24:52 -0400

    image should load now

--- frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
+++ frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
 
 import Image from 'next/image';
 import PizzaPartyAreaController from '../../../../classes/interactable/PizzaPartyAreaController';
+import background from '../../../../../public/assets/pizza-party/background.png';
 
 export type PizzaPartyGameProps = {
   gameAreaController: PizzaPartyAreaController;
@@ -13,8 +12,7 @@ export type PizzaPartyGameProps = {
 export default function PizzaPartyGame({ gameAreaController }: PizzaPartyGameProps): JSX.Element {
   return (
     <div>
-      <Image src='/assets/pizza-party/background.png' alt='Pizza Party Game' layout='fill' />
+      <Image src={background} alt='Pizza Party Game' layout='fill' />
     </div>
   );
 }
->>>>>>> 2a3f2abd3c6897d7b80d10eb5fefe120aa37a24f
