1a01056d83d980ecc4e5053250880312f4914724
sho-r24 <rakhangi.s@northeastern.edu>
2024-04-06 18:22:13 -0400

    commit before i git pull

--- frontend/src/components/Town/interactables/PizzaParty/Customer.tsx
+++ frontend/src/components/Town/interactables/PizzaParty/Customer.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Image from 'next/image';
 import { Customer as CustomerType, Order as OrderType } from '../../../../types/CoveyTownSocket';
 import * as customerSprite from '../../../../../public/assets/pizza-party/customer.png';
-import React from 'react';
 import { useState } from 'react';
 import Order from './Order';
 
 export type CustomerProps = {
   customer: CustomerType;
 };
-
 
 const mockOrder: OrderType = {
   pizzas: [
@@ -19,12 +17,12 @@ const mockOrder: OrderType = {
         {
           id: 0,
           kind: 'pepperoni',
-          appliedOnPizza:false
+          appliedOnPizza: false,
         },
         {
           id: 0,
-          kind: "anchovies",
-          appliedOnPizza:false
+          kind: 'anchovies',
+          appliedOnPizza: false,
         },
       ],
       cooked: false,
@@ -33,15 +31,13 @@ const mockOrder: OrderType = {
   ],
 };
 
-
 export default function Customer({ customer }: CustomerProps): JSX.Element {
   const [currentCustomer, setCurrentCustomer] = useState<CustomerType>(customer);
   return (
     <div>
+      <Order order={mockOrder} />
       <Image src={customerSprite} alt='Customer' />
       <div style={{ position: 'relative', top: '-10px' }}></div>
-        <Order order={mockOrder} />
-      </div>
     </div>
   );
 }
