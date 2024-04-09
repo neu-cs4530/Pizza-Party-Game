bea6fe232c977b23c2dce795ab9286c1e8ec842f
jimballl <kimball.j@northeastern.edu>
2024-04-09 13:03:20 -0400

    fixed topping image issues, fix pizza location, modified background image, got trashcan to work, fixed customer dissappearing issue

--- frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
+++ frontend/src/components/Town/interactables/PizzaParty/PizzaPartyGame.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-
+import {Pizza, Pizza as PizzaType} from '../../../../types/CoveyTownSocket';
 import Image from 'next/image';
 import PizzaPartyAreaController from '../../../../classes/interactable/PizzaPartyAreaController';
-import Pizza from './Pizza';
+import PizzaSprite from './Pizza';
 import Customer from './Customer';
 import {
   Customer as CustomerType,
@@ -13,10 +13,24 @@ import { ToppingOptions } from '../../../../types/CoveyTownSocket';
 import ToppingTray from './ToppingTray';
 import Oven from './Oven';
 import Trash from './Trash';
+import { chakra, Container } from '@chakra-ui/react';
 
 export type PizzaPartyGameProps = {
   gameAreaController: PizzaPartyAreaController;
 };
+
+/**
+ * A component that renders the Pizza Party game
+ */
+const StyledPizzaGameBoard = chakra(Container, {
+  baseStyle: {
+    display: 'flex',
+    width: '400px',
+    height: '400px',
+    padding: '5px',
+    flexWrap: 'wrap',
+  },
+});
 
 const TOPPINGS_LIST: ToppingOptions[] = [
   'pepperoni',
@@ -31,29 +45,39 @@ const TOPPINGS_LIST: ToppingOptions[] = [
 // To-Do: Add controller functionality in here
 export default function PizzaPartyGame({ gameAreaController }: PizzaPartyGameProps): JSX.Element {
   const [currentCustomers, setCurrentCustomers] = useState(gameAreaController.currentCustomers);
-  const [currentPizza, setCurrentPizza] = useState(gameAreaController.currentPizza);
+  const [currentPizza, setCurrentPizza] = useState<Pizza | undefined>(gameAreaController.currentPizza);
+  const [currentToppings, setCurrentToppings] = useState<ToppingType[] | undefined>(currentPizza?.toppings);
   const [currentScore, setCurrentScore] = useState(gameAreaController.currentScore);
   const [currentGame, setCurrentGame] = useState(gameAreaController.game);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const index = gameAreaController.findEmptySeat();
-      if (
-        index !== -1 &&
-        index !== undefined &&
-        currentCustomers !== undefined &&
-        gameAreaController.currentCustomers !== undefined
-      ) {
-        const newCustomers = [...currentCustomers];
-        const customer = gameAreaController.generateRandomCustomer();
-        newCustomers[index] = customer;
-        gameAreaController.currentCustomers[index] = customer;
-        setCurrentCustomers(newCustomers);
-        console.log('hello');
-        console.log(currentCustomers);
-      }
+      // if (currentCustomers && currentCustomers.length >= 3) {
+      //   clearInterval(intervalId);
+      // } else {
+        const index = gameAreaController.findEmptySeat();
+        if (
+          index !== -1 &&
+          index !== undefined &&
+          currentCustomers !== undefined &&
+          gameAreaController.currentCustomers !== undefined
+        ) {
+          const newCustomers = [...currentCustomers];
+          const customer = gameAreaController.generateRandomCustomer();
+          newCustomers[index] = customer;
+          gameAreaController.currentCustomers[index] = customer;
+          setCurrentCustomers(newCustomers);
+        }
+        gameAreaController.addListener('customerChanged', setCurrentCustomers);
+
     }, 3000);
-    gameAreaController.addListener('customerChanged', setCurrentCustomers);
+    return () => {
+    gameAreaController.removeListener('customerChanged', setCurrentCustomers);  
+    clearInterval(intervalId);
+    };
+  }, [gameAreaController, currentCustomers]);
+
+  useEffect(() => {
     gameAreaController.addListener('pizzaChanged', setCurrentPizza);
     gameAreaController.addListener('scoreChanged', setCurrentScore);
     gameAreaController.addListener('gameChanged', setCurrentGame);
@@ -62,9 +86,8 @@ export default function PizzaPartyGame({ gameAreaController }: PizzaPartyGamePro
       gameAreaController.removeListener('pizzaChanged', setCurrentPizza);
       gameAreaController.removeListener('scoreChanged', setCurrentScore);
       gameAreaController.removeListener('gameChanged', setCurrentGame);
-      clearInterval(intervalId);
     };
-  }, [currentGame, currentCustomers, currentPizza, gameAreaController]);
+  }, [currentGame, currentPizza, gameAreaController]);
 
   function applyTopping(topp: ToppingOptions): void {
     const top = {
@@ -78,56 +101,58 @@ export default function PizzaPartyGame({ gameAreaController }: PizzaPartyGamePro
       customer: undefined,
       gamePiece: 'placeTopping',
     });
-    console.log(currentPizza);
   }
+  function handleTrashClick(): void {
+    console.log("Trash clicked");
+    gameAreaController.makeMove({
+      gamePiece: 'throwOut',
+      pizza: currentPizza,
+      customer: undefined,
+    });
+    console.log("move made");
+  };
 
   return (
-    <div>
-      <div>
-        <Image
-          src={'/assets/pizza-party/background.png'}
-          alt='Pizza Party Game'
-          layout='fill'
-          style={{ position: 'absolute', top: 0, left: 0 }}
-        />
-        <div style={{ position: 'absolute', top: 425, left: 55, width: '100%', height: '100%' }}>
-          <Pizza pizza={currentPizza} toppings={currentPizza?.toppings} />
-        </div>
-        <div style={{ display: 'flex', position: 'absolute', left: 7 }}>
-          {currentCustomers?.map((customer, index) =>
-            customer.name !== 'Empty' ? (
-              <div style={{ marginRight: 30 }} key={index}>
-                <Customer customer={customer} />
-              </div>
-            ) : (
-              <div style={{ marginRight: 30 }} key={index}></div>
-            ),
-          )}
-        </div>
-        <div style={{ position: 'absolute', top: 350, left: 480 }}>
-          <Oven />
-        </div>
-        <div style={{ position: 'absolute', top: 430, left: 410 }}>
-          <Trash />
-        </div>
-        <div style={{ display: 'flex', position: 'absolute', left: 7, top: 350 }}>
-          {TOPPINGS_LIST.map((topping, index) => {
-            return (
-              <div key={index} style={{ marginRight: 30 }} onClick={() => applyTopping(topping)}>
-                <ToppingTray topping={topping} />
-              </div>
-            );
-          })}
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', position: 'absolute', top: 400 }}>
-          <ToppingTray topping={'sauce'} />
-          <ToppingTray topping={'cheese'} />
-        </div>
+    <StyledPizzaGameBoard>
+      <Image
+        src={'/assets/pizza-party/background.png'}
+        alt='Pizza Party Game'
+        layout='fill'
+        style={{ position: 'absolute', top: 0, left: 0 }}
+      />
+      <div style={{ position: 'absolute', top: 450, left:55,  width: '100%', height: '100%'}}>
+        <PizzaSprite pizza={currentPizza} toppings={currentPizza?.toppings} />
       </div>
-      <div>
-        <div>Instructions</div>
-        <div></div>
+      <div style={{ display: 'flex', position: 'absolute', left: 7 }}>
+        {currentCustomers?.map((customer, index) =>
+          customer.name !== 'Empty' ? (
+            <div style={{ marginRight: 30 }} key={index}>
+              <Customer customer={customer} />
+            </div>
+          ) : (
+            <div style={{ marginRight: 30 }} key={index}></div>
+          ),
+        )}
       </div>
-    </div>
+      <div style={{ position: 'absolute', top: 350, left: 480 }}>
+        <Oven />
+      </div>
+      <div style={{ position: 'absolute', top: 430, left: 410 }}>
+        <Trash onClick={() => handleTrashClick()}/>
+      </div>
+      <div style={{ display: 'flex', position: 'absolute', left: 7, top: 350 }}>
+        {TOPPINGS_LIST.map((topping, index) => {
+          return (
+            <div key={index} style={{ marginRight: 30 }} onClick={() => applyTopping(topping)}>
+              <ToppingTray topping={topping} />
+            </div>
+          );
+        })}
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', position: 'absolute', top: 400 }}>
+        <ToppingTray topping={'sauce'} />
+        <ToppingTray topping={'cheese'} />
+      </div>
+    </StyledPizzaGameBoard>
   );
 }
--- frontend/src/components/Town/interactables/PizzaParty/Trash.tsx
+++ frontend/src/components/Town/interactables/PizzaParty/Trash.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import Image from 'next/image';
 
-export default function Trash(): JSX.Element {
+interface TrashProps {
+  onClick: () => void;
+}
+
+export default function Trash({ onClick }: TrashProps): JSX.Element {
   return (
-    <button onClick={() => console.log('Trash clicked')}>
+    <button onClick={onClick}>
       <Image src={'/assets/pizza-party/trash.jpg'} height={120} width={60} />
     </button>
   );
